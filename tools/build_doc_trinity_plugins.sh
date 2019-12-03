e0613cb7e3b1ea873fc210d39ee6f76325e22654
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-03 09:24:31 +0800

    Trinity plugins documentation using Typedoc

--- tools/build_doc_trinity_plugins.sh
+++ tools/build_doc_trinity_plugins.sh
@@ -2,7 +2,7 @@
 
 dir=$(pwd)
 mkdir -p ../static/references/trinity_plugins
-cd ../../Elastos.Trinity.ToolChains/doc
+cd ../../Elastos.Trinity/ToolChains/doc
 ./gen_plugins_doc.sh
 cp -r out/* $dir/../static/references/trinity_plugins/
 cd $dir
